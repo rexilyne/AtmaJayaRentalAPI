446c501c5295e2cba2ca40f349eb9ae678ac90f2
ASUS <tfadriangawa@gmail.com>
2022-04-12 14:18:30 +0700

    Update Routes

--- routes/api.php
+++ routes/api.php
@@ -29,17 +29,17 @@ use App\Http\Controllers\Api\PromoController;
 
 Route::controller(CustomerController::class)->group(function () {
     Route::get('/customer', 'index');
-    Route::get('/customer/{id}', 'show');
-    Route::post('/customer', 'store');
-    Route::put('/customer/{id}', 'update');
-    Route::delete('/customer/{id}', 'destroy');
+    Route::get('/customer/show/{id}', 'show');
+    Route::post('/customer/store', 'store');
+    Route::put('/customer/update/{id}', 'update');
+    Route::delete('/customer/delete/{id}', 'destroy');
 });
 
 Route::controller(DetailJadwalController::class)->group(function () {
     Route::get('/detailjadwal', 'index');
     Route::get('/detailjadwal/get/idpegawai/{id}', 'showByIdPegawai');
     Route::get('/detailjadwal/get/idjadwal/{id}', 'showByIdJadwal');
-    Route::post('/detailjadwal', 'store');
+    Route::post('/detailjadwal/store', 'store');
     Route::put('/detailjadwal/update/idpegawai/{id}', 'updateByIdPegawai');
     Route::put('/detailjadwal/update/idjadwal/{id}', 'updateByIdJadwal');
     Route::delete('/detailjadwal/delete/idpegawai/{id}', 'destroyByIdPegawai');
@@ -49,27 +49,27 @@ Route::controller(DetailJadwalController::class)->group(function () {
 
 Route::controller(DriverController::class)->group(function () {
     Route::get('/driver', 'index');
-    Route::get('/driver/{id}', 'show');
-    Route::post('/driver', 'store');
-    Route::put('/driver/{id}', 'update');
-    Route::delete('/driver/{id}', 'destroy');
+    Route::get('/driver/show/{id}', 'show');
+    Route::post('/driver/store', 'store');
+    Route::put('/driver/update/{id}', 'update');
+    Route::delete('/driver/delete/{id}', 'destroy');
     Route::get('/driver/showavailable', 'showAvailable');
 });
 
 Route::controller(JadwalController::class)->group(function () {
     Route::get('/jadwal', 'index');
-    Route::get('/jadwal/{id}', 'show');
-    Route::post('/jadwal', 'store');
-    Route::put('/jadwal/{id}', 'update');
-    Route::delete('/jadwal/{id}', 'destroy');
+    Route::get('/jadwal/show/{id}', 'show');
+    Route::post('/jadwal/store', 'store');
+    Route::put('/jadwal/update/{id}', 'update');
+    Route::delete('/jadwal/delete/{id}', 'destroy');
 });
 
 Route::controller(MobilController::class)->group(function () {
     Route::get('/mobil', 'index');
-    Route::get('/mobil/{id}', 'show');
-    Route::post('/mobil', 'store');
-    Route::put('/mobil/{id}', 'update');
-    Route::delete('/mobil/{id}', 'destroy');
+    Route::get('/mobil/show/{id}', 'show');
+    Route::post('/mobil/store', 'store');
+    Route::put('/mobil/update/{id}', 'update');
+    Route::delete('/mobil/delete/{id}', 'destroy');
     Route::get('/mobil/showavailable', 'showAvailabe');
     Route::get('/mobil/showkontrakakanhabis', 'showKontrakAkanHabis');
     Route::put('/mobil/updateperiodekontrak/{id}', 'updatePeriodeKontrak');
@@ -77,35 +77,35 @@ Route::controller(MobilController::class)->group(function () {
 
 Route::controller(PegawaiController::class)->group(function () {
     Route::get('/pegawai', 'index');
-    Route::get('/pegawai/{id}', 'show');
-    Route::post('/pegawai', 'store');
-    Route::put('/pegawai/{id}', 'update');
-    Route::delete('/pegawai/{id}', 'destroy');
+    Route::get('/pegawai/show/{id}', 'show');
+    Route::post('/pegawai/store', 'store');
+    Route::put('/pegawai/update/{id}', 'update');
+    Route::delete('/pegawai/delete/{id}', 'destroy');
 });
 
 Route::controller(PemilikMobilController::class)->group(function () {
     Route::get('/pemilikmobil', 'index');
-    Route::get('/pemilikmobil/{id}', 'show');
-    Route::post('/pemilikmobil', 'store');
-    Route::put('/pemilikmobil/{id}', 'update');
-    Route::delete('/pemilikmobil/{id}', 'destroy');
+    Route::get('/pemilikmobil/show/{id}', 'show');
+    Route::post('/pemilikmobil/store', 'store');
+    Route::put('/pemilikmobil/update/{id}', 'update');
+    Route::delete('/pemilikmobil/delete/{id}', 'destroy');
 });
 
 Route::controller(PenyewaanController::class)->group(function () {
     Route::get('/penyewaan', 'index');
-    Route::get('/penyewaan/get/idpenyewaan/{id}', 'showByIdPenyewaan');
-    Route::get('/penyewaan/get/idcustomer/{id}', 'showByIdCustomer');
-    Route::get('/penyewaan/get/iddriver/{id}', 'showByIdDriver');
-    Route::post('/penyewaan', 'store');
-    Route::put('/penyewaan/{id}', 'update');
-    Route::delete('/penyewaan/{id}', 'destroy');
+    Route::get('/penyewaan/show/idpenyewaan/{id}', 'showByIdPenyewaan');
+    Route::get('/penyewaan/show/idcustomer/{id}', 'showByIdCustomer');
+    Route::get('/penyewaan/show/iddriver/{id}', 'showByIdDriver');
+    Route::post('/penyewaan/store', 'store');
+    Route::put('/penyewaan/update/{id}', 'update');
+    Route::delete('/penyewaan/delete/{id}', 'destroy');
 });
 
 Route::controller(PromoController::class)->group(function () {
     Route::get('/promo', 'index');
-    Route::get('/promo/{id}', 'show');
-    Route::post('/promo', 'store');
-    Route::put('/promo/{id}', 'update');
-    Route::delete('/promo/{id}', 'destroy');
+    Route::get('/promo/show/{id}', 'show');
+    Route::post('/promo/store', 'store');
+    Route::put('/promo/update/{id}', 'update');
+    Route::delete('/promo/delete/{id}', 'destroy');
     Route::get('/promo/showactive', 'showActive');
 });
